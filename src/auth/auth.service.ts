1f3f649d499dbfba9fe683b349b3e9bc95432975
Shivam Rastogi <shivamraspro@gmail.com>
2025-08-08 17:48:01 +0530

    feat: add @nestjs/config to read env variables

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,21 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
 import { User } from '../users/types/user.interface';
 import { LoginDto, SignupDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {}
 
   signup(signupDto: SignupDto) {
     const { email, password } = signupDto;
     const user = { email, password };
+    const salt = this.configService.get<string>('AUTH_SALT')!;
+    console.log('auth salt', salt, typeof salt);
     return this.usersService.createOne(user as User);
   }
 
