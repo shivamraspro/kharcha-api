e585c146c1b9087cb350cc0450952d23aec53663
Shivam Rastogi <shivamraspro@gmail.com>
2025-08-13 00:23:55 +0530

    feat: use db module for db configurations (#19)
    - set autoLoadEntities to true

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,26 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // Library Modules
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 // Project Modules
+import { DbModule } from './db/db.module';
 import { UsersModule } from './users/users.module';
-// Project Entities
-import { User } from './users/user.entity';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({}),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT!),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User],
-    }),
-    UsersModule,
-  ],
+  imports: [ConfigModule.forRoot({}), DbModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/db/db.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        // Configure DB/TypeORM here
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT')!),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+})
+export class DbModule { }
