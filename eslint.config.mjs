3d4cc132ebf77e15876209d3ab6a74b2b1d0a4fa
Shivam Rastogi <shivamraspro@gmail.com>
2025-08-16 01:48:09 +0530

    chore: replace prettier with eslint-stylistic (#26)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import stylystic from '@stylistic/eslint-plugin';
 
 export default tseslint.config(
   {
@@ -10,7 +10,6 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintConfigPrettier,
   {
     languageOptions: {
       globals: {
@@ -24,6 +23,23 @@ export default tseslint.config(
       },
     },
   },
+  stylystic.configs.customize({
+    // This is a factory config and can only be used to set some general styles
+    // View the source code to see how this factory config translates to eslint rules
+    // https://github.com/eslint-stylistic/eslint-stylistic/blob/main/packages/eslint-plugin/configs/customize.ts
+    // Full reference: https://eslint.style/rules/quote-props
+    arrowParens: true,
+    blockSpacing: true,
+    braceStyle: '1tbs',
+    commaDangle: 'always-multiline',
+    indent: 2,
+    jsx: true,
+    pluginName: '@stylistic',
+    quoteProps: 'consistent-as-needed',
+    quotes: 'single',
+    semi: true,
+    severity: 'warn',
+  }),
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
